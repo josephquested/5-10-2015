296b387cec05585c480797936a7288400a831bd6
josephquested <josephquested@gmail.com>
2017-02-27 16:13:06 +1300

    unity button and restart working

---
+++ Assets/Scripts/MineButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MineButton : Clickable {
+	public override void Click ()
+	{
+		print("boom");
+	}
+}
