de067608e9e14397b5fca55530a0fb2f1648ae67
josephquested <josephquested@gmail.com>
2017-02-27 16:36:46 +1300

    search button getting close

---
+++ Assets/Scripts/SearchButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchButton : Clickable {
+	public float force;
+
+	public override void Click ()
+	{
+		GetComponent<AudioSource>().Play();
+		StartCoroutine(StartRotation());
+		Rigidbody rb = GetComponent<Rigidbody>();
+		Vector3 movement = new Vector3(-1, 2, 0);
+		rb.AddForce(movement * force);
+	}
+
+	IEnumerator StartRotation ()
+	{
+		int i = 0;
+		Rigidbody rb = GetComponent<Rigidbody>();
+		while (i < 100)
+		{
+			yield return null;
+			Vector3 rotation = new Vector3(10, 0);
+			Quaternion deltaRotation = Quaternion.Euler(rotation * Time.deltaTime * 10);
+			rb.MoveRotation(rb.rotation * deltaRotation);
+		}
+	}
+}
