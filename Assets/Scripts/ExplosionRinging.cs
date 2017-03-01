96203899fe79cbc17d88ba3dc306ed5a9c976e56
josephquested <josephquested@gmail.com>
2017-03-02 12:32:44 +1300

    twisted camera and title card finished

--- Assets/Scripts/ExplosionRinging.cs
+++ Assets/Scripts/ExplosionRinging.cs
@@ -16,13 +16,13 @@ public class ExplosionRinging : MonoBehaviour {
 	{
 		while (ringAudio.volume < 1)
 		{
-			ringAudio.volume += 0.01f;
+			ringAudio.volume += 0.15f;
 			yield return new WaitForSeconds(0.1f);
 		}
 		yield return new WaitForSeconds(highTime);
 		while (ringAudio.volume > 0)
 		{
-			ringAudio.volume -= 0.01f;
+			ringAudio.volume -= 0.15f;
 			yield return new WaitForSeconds(0.1f);
 		}
 	}
--- Assets/Scripts/PlayerExplode.cs
+++ Assets/Scripts/PlayerExplode.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
 
 public class PlayerExplode : MonoBehaviour {
+	public float getupTime;
 	public void Explode ()
 	{
 		GetComponent<Animator>().enabled = true;
@@ -12,7 +13,7 @@ public class PlayerExplode : MonoBehaviour {
 
 	IEnumerator ExplosionCoroutine ()
 	{
-		yield return new WaitForSeconds(5.5f);
+		yield return new WaitForSeconds(getupTime);
 		Destroy(GetComponent<Animator>());
 		GetComponent<FirstPersonController>().enabled = true;
 	}
