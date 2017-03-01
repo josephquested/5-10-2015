f923842d1c9c413aac0a4503bdfee0f5dee1ee06
josephquested <josephquested@gmail.com>
2017-03-02 12:08:23 +1300

    add explosion ring

---
+++ Assets/Scripts/ExplosionRinging.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionRinging : MonoBehaviour {
+	AudioSource ringAudio;
+	public float highTime;
+
+	void Start ()
+	{
+		ringAudio = GetComponent<AudioSource>();
+		StartCoroutine(RingRoutine());
+	}
+
+	IEnumerator RingRoutine ()
+	{
+		while (ringAudio.volume < 1)
+		{
+			ringAudio.volume += 0.01f;
+			yield return new WaitForSeconds(0.1f);
+		}
+		yield return new WaitForSeconds(highTime);
+		while (ringAudio.volume > 0)
+		{
+			ringAudio.volume -= 0.01f;
+			yield return new WaitForSeconds(0.1f);
+		}
+	}
+}
