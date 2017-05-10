fa529c88f908a8a2df7325344b02dbe855e6ce27
Joseph Quested <josephquested@gmail.com>
2017-05-10 16:58:02 +1200

    add trigger audio source

---
+++ Assets/Scripts/PosterLight.cs
@@ -0,0 +1,17 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PosterLight : MonoBehaviour {
+
+	// SYSTEM //
+
+	public Light posterLight;
+	public float lightMin;
+	public float lightMax;
+
+	void Update ()
+	{
+		posterLight.intensity = Random.Range(lightMin, lightMax);
+	}
+}
--- Assets/Scripts/RapeYourEnemiesTrigger.cs
+++ Assets/Scripts/RapeYourEnemiesTrigger.cs
@@ -7,12 +7,37 @@ public class RapeYourEnemiesTrigger : MonoBehaviour {
 	// SYSTEM //
 
 	public GameObject poster;
+	public GameObject wall;
 
 	void OnTriggerEnter (Collider collider)
 	{
 		if (collider.tag == "Player")
 		{
-			poster.SetActive(true);
+			GetComponent<AudioSource>().Play();
+			StartCoroutine(FlickerOnRoutine());
 		}
+	}
+
+	void ActivatePoster (bool state)
+	{
+		poster.SetActive(state);
+		wall.SetActive(state);
+	}
+
+	IEnumerator FlickerOnRoutine ()
+	{
+		ActivatePoster(true);
+		yield return new WaitForSeconds(0.1f);
+		ActivatePoster(false);
+		yield return new WaitForSeconds(0.1f);
+		ActivatePoster(true);
+		yield return new WaitForSeconds(0.1f);
+		ActivatePoster(false);
+		yield return new WaitForSeconds(0.1f);
+		ActivatePoster(true);
+		yield return new WaitForSeconds(0.1f);
+		ActivatePoster(false);
+		yield return new WaitForSeconds(0.1f);
+		ActivatePoster(true);
 	}
 }
