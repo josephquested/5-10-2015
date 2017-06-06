f17567c7015381372cd11bbea387a2dbfd4abede
Joseph Quested <josephquested@gmail.com>
2017-06-06 12:28:14 +1200

    add money room door and trigger

---
+++ Assets/Scripts/ThroneTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThroneTrigger : MonoBehaviour {
+
+	public GameObject doorObj;
+
+	void OnTriggerEnter (Collider collider)
+	{
+		if (collider.tag == "Player")
+		{
+			StartCoroutine(DoorRoutine());
+		}
+	}
+
+	IEnumerator DoorRoutine ()
+	{
+		doorObj.SetActive(true);
+		doorObj.GetComponent<Animator>().SetTrigger("Shut");
+		yield return null;
+	}
+}
